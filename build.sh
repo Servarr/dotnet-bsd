9fba3eda84bedf6dbce9c2b429d8b339d6b86d4e
ta264 <ta264@users.noreply.github.com>
2021-02-07 22:27:08 +0000

    Remove unnecessary patching

--- build.sh
+++ build.sh
@@ -2,24 +2,32 @@
 
 set -e
 
+RUNTIMETAG=v5.0.2
+SDKTAG=v5.0.102
+
 ## Runtime
-git clone --depth 1 --branch v5.0.2 https://github.com/dotnet/runtime.git
+git clone --depth 1 --branch $RUNTIMETAG https://github.com/dotnet/runtime.git
 
 DOTNET_DOCKER_TAG="mcr.microsoft.com/dotnet-buildtools/prereqs:$(curl -s https://raw.githubusercontent.com/dotnet/versions/master/build-info/docker/image-info.dotnet-dotnet-buildtools-prereqs-docker-master.json | jq -r '.repos[0].images[] | select(.platforms[0].dockerfile | contains("freebsd/11")) | .platforms[0].simpleTags[0]')"
 docker run -e ROOTFS_DIR=/crossrootfs/x64 -v $(pwd)/runtime:/runtime $DOTNET_DOCKER_TAG /runtime/build.sh -c Release -cross -os freebsd
 
 ## AspNetCore
-git clone --recursive --depth 1 --branch v5.0.2 https://github.com/dotnet/aspnetcore.git
+git clone --recursive --depth 1 --branch $RUNTIMETAG https://github.com/dotnet/aspnetcore.git
 git -C aspnetcore apply ../dotnet-bsd/patches/aspnetcore/0001-freebsd-support.patch
+dotnet nuget add source ../runtime/artifacts/packages/Release/Shipping --name runtime --configfile aspnetcore/NuGet.config
 
 mkdir -p aspnetcore/artifacts/obj/Microsoft.AspNetCore.App.Runtime
-cp runtime/artifacts/packages/Release/Shipping/dotnet-runtime-5.0.2-freebsd-x64.tar.gz aspnetcore/artifacts/obj/Microsoft.AspNetCore.App.Runtime
+cp runtime/artifacts/packages/Release/Shipping/dotnet-runtime-5.*-freebsd-x64.tar.gz aspnetcore/artifacts/obj/Microsoft.AspNetCore.App.Runtime
 
 aspnetcore/build.sh -c Release -ci --os-name freebsd -pack -nobl /p:CrossgenOutput=false /p:OfficialBuildId=$(date +%Y%m%d)-99
 
 ## Installer
-git clone --depth 1 --branch v5.0.102 https://github.com/dotnet/installer.git
+git clone --depth 1 --branch $SDKTAG https://github.com/dotnet/installer.git
 git -C installer apply ../dotnet-bsd/patches/installer/0001-freebsd-support.patch
+dotnet nuget remove source msbuild --configfile installer/NuGet.config
+dotnet nuget remove source nuget-build --configfile installer/NuGet.config
+dotnet nuget add source ../runtime/artifacts/packages/Release/Shipping --name runtime --configfile installer/NuGet.config
+dotnet nuget add source ../aspnetcore/artifacts/packages/Release/Shipping --name aspnetcore --configfile installer/NuGet.config
 
 mkdir -p installer/artifacts/obj/redist/Release/downloads/
 cp runtime/artifacts/packages/Release/Shipping/dotnet-runtime-*-freebsd-x64.tar.gz installer/artifacts/obj/redist/Release/downloads/
